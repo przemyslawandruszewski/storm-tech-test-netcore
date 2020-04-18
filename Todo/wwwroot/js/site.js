25d8ab378c23468a3139bb97a36b634f10cfe961
Przemysław Andruszewski <60489735+przemyslawandruszewski@users.noreply.github.com>
2020-04-18 20:57:52 +0200

    Task-5 site.js file changes attached.

--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function() {
+    $('.auto-submit').on('click', function(e) {
+        const form = e.target.closest('form');
+        form.submit();
+    });
+});
